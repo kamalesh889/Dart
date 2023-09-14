6eeb8211b31d52dfefb1efa0bcdcac86e4fb9402
kamalesh889 <kamaleshmohapatra889@gmail.com>
2023-09-14 23:02:03 +0530

    first commit

---
+++ first.dart
@@ -0,0 +1,18 @@
+void main() {
+  print("Hello first dart program");
+  variablecheck();
+}
+
+variablecheck() {
+  var a = 123;
+  var b = "checker";
+  var c = [1, 2, 3, 4];
+  var d = ["a", "b", "c", "d"];
+  var mp = {"ram": true, "others": 0};
+
+  print(a);
+  print(b);
+  print(c);
+  print(d);
+  print(mp);
+}
